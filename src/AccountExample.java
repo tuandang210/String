ba5de57b9ccce9504e95670bd2dfc1aaa49aa71d
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-29 09:49:36 +0700

    [Thực hành] Validate account

---
+++ src/AccountExample.java
@@ -0,0 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AccountExample {
+    private static final String ACCOUNT_REGEX = "^[_a-z0-9]{6,}$";
+
+    public static final String[] validAccount = new String[]{"123abc_", "_abc123", "______", "123456", "abcdefgh"};
+    public static final String[] invalidAccount = new String[]{".@", "12345", "1234_", "abcde"};
+
+
+    public AccountExample() {
+    }
+
+    public boolean validate(String regex) {
+        Pattern pattern = Pattern.compile(ACCOUNT_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+        AccountExample accountExample = new AccountExample();
+        for (String account : validAccount) {
+            boolean isvalid = accountExample.validate(account);
+            System.out.println("Account is " + account + " is valid: " + isvalid);
+        }
+        for (String account : invalidAccount) {
+            boolean isvalid = accountExample.validate(account);
+            System.out.println("Account is " + account + " is valid: " + isvalid);
+        }
+    }
+}
